926b0ff53677a33da26917e1c33912c6c39fbb65
MoulikB <moulikbhatia@icloud.com>
2025-10-24 22:12:20 -0500

    Phase 1

--- src/main/java/COMP2450/model/PrintReview.java
+++ src/main/java/COMP2450/model/PrintReview.java
@@ -5,12 +5,14 @@ public class PrintReview {
     Returns the review as a string
      */
     public static void printReview(Review review) {
+        System.out.println("==============");
         String output = "";
         output += "User: " + review.user().getUsername();
         output += "\nMedia: " + review.media().getTitle();
         output += "\nComment: " + review.comment();
         output += "\nStars: " + review.stars();
         System.out.println(output);
+        System.out.println("==============");
     }
 
     public static void printReviews(MediaInterface media) {
